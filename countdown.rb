1e6737d6278534fc90957d7ff67d2eda896f7ab6
Huy Do <huyddo40@gmail.com>
2019-06-18 00:57:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 #write your code here
 
 def countdown(number)
- 
+
   while number > 0 do
     puts "#{number} SECOND(S)!"
     number -=1
   end
   return "HAPPY NEW YEAR!"
-  
+
 end
 
 
 def countdown_with_sleep(number)
   countdown(number)
-  
-  sleep(5) 
+
+  sleep(5)
 end
